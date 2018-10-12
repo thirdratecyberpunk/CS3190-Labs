7ba5f2cb18ac16459bac7cbf0fb35d179e62ec68
thirdratecyberpunk <blackbul@aston.ac.uk>
2018-10-12 16:48:48 +0100

    Added random antenna generation.

--- src/AntennaArrayProblem.java
+++ src/AntennaArrayProblem.java
@@ -1,4 +1,11 @@
+import java.util.Arrays;
+import java.util.Random;
 
+/**
+ * Implementation of the antenna array problem used in CS3910.
+ * @author lewis
+ *
+ */
 public class AntennaArrayProblem {
 
 	public static void main(String[] args) {
@@ -7,13 +14,39 @@ public class AntennaArrayProblem {
 	
 	public AntennaArrayProblem() {
 		AntennaArray aa = new AntennaArray(3, 90);
-		double[] design = {0.2, 0.8, 1.5};
-		System.out.println(aa.is_valid(design));
+		double[] design = getRandomValidSolution(aa);
+		System.out.println("Antenna Array design problem");
+		System.out.println(Arrays.toString(design));
 		System.out.println(aa.evaluate(design));
 	}
 	
-	private double[] getRandomSolution(AntennaArray aa) {
-		return null;
+	/**
+	 * Returns a valid double of positions given an AntennaArray instance.
+	 * @param aa
+	 * @return
+	 */
+	private double[] getRandomValidSolution(AntennaArray aa) {
+		boolean valid = false;
+		double[] newRandomSolution = null;
+		double[][] bounds = aa.bounds();
+		double maxApertureSize = (bounds.length) / 2.0;
+		while (valid == false) {
+			newRandomSolution = new double[bounds.length];
+			// generates a number between the bounds for each 
+			for (int i = 0; i < bounds.length - 1; i++) {
+				double start = bounds[i][0];
+				double end = bounds[i][1];
+				double random = new Random().nextDouble();
+				double result = start + (random * (end - start));
+				newRandomSolution[i] = result;
+			}
+			// sets the final result to the maximum aperture size
+			newRandomSolution[bounds.length - 1] = maxApertureSize;
+			if (aa.is_valid(newRandomSolution)) {
+				valid = true;
+			}
+		}
+		return newRandomSolution;
 	}
 
 }
